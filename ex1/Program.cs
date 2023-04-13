729600f6cfc922c9df2491ee4db4823927a57e1d
Ro <adm.031@ya.ru>
2023-04-13 12:21:22 +0300

    Задача 1

---
+++ ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Выполнить с помощью рекурсии.Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+
+Console.Clear();
+UserData("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(Numbers(N,1));
+
+//----------Метод
+string Numbers(int N, int end){
+    if (N==1) return N.ToString();
+    return (N + " " + Numbers(N-1, end));
+}
+
+//---------Метод опроса пользователя
+void UserData(string message){
+    System.Console.WriteLine($"{message}");
+}
